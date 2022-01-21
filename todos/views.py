fc39673eff8a36b6917d5ce09ebc8361a25dc777
charl <charl@DESKTOP-A4DK2O9>
2022-01-21 15:32:03 +0000

    added permissions class

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
 from todos.models import Todo
+from todos.permissions import TodoPermissions
 from todos.serializers import TodoSerializer
 
-# Create your views here.
-class TodoViewSet(viewsets.ModelViewSet): # new
+
+class TodoViewSet(viewsets.ModelViewSet):
+    permissions_class = (TodoPermissions, )
     queryset = Todo.objects.all().order_by('-id')
     serializer_class = TodoSerializer
+
+    
