647431af7dd98570f936e51ba981921cab03c451
charl <charl@DESKTOP-A4DK2O9>
2022-01-21 15:16:12 +0000

    added to model and added api-auth

--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 
 class Todo(models.Model):
-    text      = models.CharField(max_length=250)
-    completed = models.BooleanField(default=False, null=True, blank=True)
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    text       = models.CharField(max_length=250)
+    completed  = models.BooleanField(default=False, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.text
--- web_project/settings.py
+++ web_project/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-'DEFAULT_PERMISSION_CLASSES': [
-'rest_framework.permissions.AllowAny',
-]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
 }
 
 MIDDLEWARE = [
